94d9e3982e8c31d71d4c2487a555c206aae9b1c5
luoop <1337939247@qq.com>
2016-10-12 13:05:55 +0800

    rewrite getflow by requests

--- web/cgi-bin/getflow.py
+++ web/cgi-bin/getflow.py
@@ -1,14 +1,24 @@
-import urllib.request
+import requests
 import re
 import json
 import time
+
 
 print("Cache-control: no-cache")
 print("Content-Type: application/json")
 print()
 
-res = urllib.request.urlopen(r'http://10.3.8.211/')
-text = res.read(1000).decode("gbk")
-flow = re.search(r"flow='(\d+)\s*'", text).group(1)
-ret = {'status': 0, 'flow': int(flow), "time": time.time()}
-print(json.dumps(ret))
+res = requests.get(r'http://10.3.8.211/')
+text = res.text
+flow = re.search(r"flow='(\d+)\s*'", text)
+if flow:
+    ret = {'status': 0, 'flow': int(flow.group(1)), "time": time.time()}
+    print(json.dumps(ret))
+else:
+    f = open("resource/form.json",'r',encoding='utf-8')
+    form = json.load(f)
+    headers = {
+        "Content-Type": "application/json",
+        "User-Agent":"ozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+    }
+    requests.post(r'http://10.3.8.211/',form,headers)
