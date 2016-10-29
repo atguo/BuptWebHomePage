83ef422f5b872ccc912ee8864f711d3638f0d8f9
luoop <1337939247@qq.com>
2016-10-29 15:09:40 +0800

    add background change and fix size

--- web/cgi-bin/getflow.py
+++ web/cgi-bin/getflow.py
@@ -15,10 +15,10 @@ if flow:
     ret = {'status': 0, 'flow': int(flow.group(1)), "time": time.time()}
     print(json.dumps(ret))
 else:
-    f = open("resource/form.json",'r',encoding='utf-8')
+    f = open(r'D:\软件\BuptWebHomePage-master\BuptWebHomePage-master\web\cgi-bin\resource\form.json',  'r',  encoding='utf-8')
     form = json.load(f)
     headers = {
         "Content-Type": "application/json",
-        "User-Agent":"ozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+        "User-Agent": "ozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
     }
-    requests.post(r'http://10.3.8.211/',form,headers)
+    requests.post(r'http://10.3.8.211/', form, headers)
